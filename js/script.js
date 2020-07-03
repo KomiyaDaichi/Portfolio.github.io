77f9ff0a8931f628be1525356b04722af5e0e12f
KomiyaDaichi <64347664+KomiyaDaichi@users.noreply.github.com>
2020-07-04 01:59:21 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 
 $(function(){
+	$(window).on('resize',function(){
+		
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.top-nav').click(function(){
 			$('.top-nav').css('border-bottom','4px solid #222');
@@ -20,9 +22,11 @@ $(function(){
 			$('.profile-nav').removeClass('active');
 		});
 		}
+	});
 });
 
 $(function(){
+	$(window).on('resize',function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.work-nav').click(function(){
 			$('.work-nav').addClass('active');
@@ -41,10 +45,12 @@ $(function(){
 			$('.skill-box').fadeOut(300);
 		});
 		}
+	});
 });
 
 
 $(function(){
+	$(window).on('resize',function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.skill-nav').click(function(){
 			$('.skill-nav').addClass('active');
@@ -63,9 +69,11 @@ $(function(){
 			$('.top-nav').css('border-bottom','none');
 		});
 		}
+	});
 });
 
 $(function(){
+	$(window).on('resize',function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.profile-nav').click(function(){
 			$('.profile').fadeIn(100);
@@ -84,6 +92,7 @@ $(function(){
 			$('.top-nav').css('border-bottom','none');
 		});
 		}
+	});
 });
 
 
@@ -110,26 +119,32 @@ $(function(){
 
 
 $(function(){
+	$(window).on('resize',function(){
 		if(window.matchMedia('(max-width:1024px)').matches){
 			$('.work-nav').click(function(){
 				$('html,body').animate({scrollTop:$('.work').offset().top},500);
 		});
 		}
+	});
 });
 
 
 $(function(){
+	$(window).on('resize load',function(){
 		if(window.matchMedia('(max-width:1024px)').matches){
 			$('.skill-nav').click(function(){
 				$('html,body').animate({scrollTop:$('.skill').offset().top},500);
 		});
 		}
+	});
 });
 
 $(function(){
+	$(window).on('resize load',function(){
 		if(window.matchMedia('(max-width:1024px)').matches){
 			$('.profile-nav').click(function(){
 				$('html,body').animate({scrollTop:$('.profile').offset().top},500);
 		});
 		}
+	});
 });
