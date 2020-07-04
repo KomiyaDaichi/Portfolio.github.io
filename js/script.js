96fb8b7200c7baa65e8e9de5df1a235e3773c1d6
KomiyaDaichi <64347664+KomiyaDaichi@users.noreply.github.com>
2020-07-05 02:15:16 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 
 $(function(){
-	$(window).load(function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.top-nav').click(function(){
 			$('.top-nav').css('border-bottom','4px solid #222');
@@ -21,11 +20,9 @@ $(function(){
 			$('.profile-nav').removeClass('active');
 		});
 		}
-		});
 });
 
 $(function(){
-	$(window).load(function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.work-nav').click(function(){
 			$('.work-nav').addClass('active');
@@ -44,12 +41,10 @@ $(function(){
 			$('.skill-box').fadeOut(300);
 		});
 		}
-		});
 });
 
 
 $(function(){
-	$(window).load(function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.skill-nav').click(function(){
 			$('.skill-nav').addClass('active');
@@ -68,11 +63,9 @@ $(function(){
 			$('.top-nav').css('border-bottom','none');
 		});
 		}
-		});
 });
 
 $(function(){
-	$(window).load(function(){
 		if(window.matchMedia('(min-width:1024px)').matches){
 			$('.profile-nav').click(function(){
 			$('.profile').fadeIn(100);
@@ -91,7 +84,6 @@ $(function(){
 			$('.top-nav').css('border-bottom','none');
 		});
 		}
-		});
 });
 
 
@@ -118,11 +110,13 @@ $(function(){
 
 
 $(function(){
+	$(window).resize(function(){
 		if(window.matchMedia('(max-width:1024px)').matches){
 			$('.work-nav').click(function(){
 				$('html,body').animate({scrollTop:$('.work').offset().top},500);
 		});
 		}
+	});
 });
 
 
