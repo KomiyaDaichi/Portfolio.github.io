365a1d0ce350706675134e57ff2ca3f7169b5fac
KomiyaDaichi <64347664+KomiyaDaichi@users.noreply.github.com>
2020-07-08 03:33:44 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(function(){
 	$(window).scroll(function(){
-	if($(window).scrollTop()>100){
+	if($(window).scrollTop()>80){
 		$('.logo').addClass('active');
 	}else{
 		$('.logo').removeClass('active');
@@ -24,20 +24,65 @@ $(function(){
 
 
 $(function(){
-			$('.work-nav').click(function(){
-				$('html,body').animate({scrollTop:$('.work').offset().top},500);
-		});
+	$('.work-nav').click(function(){
+		$('html,body').animate({scrollTop:$('.work').offset().top},500);
+	});
 });
 
 
 $(function(){
-			$('.skill-nav').click(function(){
-				$('html,body').animate({scrollTop:$('.skill').offset().top},500);
-		});
+	$('.skill-nav').click(function(){
+		$('html,body').animate({scrollTop:$('.skill').offset().top},500);
+	});
 });
 
 $(function(){
-			$('.profile-nav').click(function(){
-				$('html,body').animate({scrollTop:$('.profile').offset().top},500);
+	$('.profile-nav').click(function(){
+		$('html,body').animate({scrollTop:$('.profile').offset().top},500);
+	});
+});
+
+
+$(function(){
+	$('.res-menu').click(function(){
+		$('.res-menu').toggleClass('active');
+		$('.nav').fadeToggle(300);
+	});
+});
+
+$(function(){
+	if(window.matchMedia('(max-width:896px)').matches){
+		$('.top-nav').click(function(){
+			$('.nav').fadeOut(300);
+			$('.res-menu').removeClass('active');
 		});
+	}
+});
+
+
+$(function(){
+	if(window.matchMedia('(max-width:896px)').matches){
+		$('.work-nav').click(function(){
+			$('.nav').fadeOut(300);
+			$('.res-menu').removeClass('active');
+		});
+	}
+});
+
+$(function(){
+	if(window.matchMedia('(max-width:896px)').matches){
+		$('.skill-nav').click(function(){
+			$('.nav').fadeOut(300);
+			$('.res-menu').removeClass('active');
+		});
+	}
+});
+
+$(function(){
+	if(window.matchMedia('(max-width:896px)').matches){
+		$('.profile-nav').click(function(){
+			$('.nav').fadeOut(300);
+			$('.res-menu').removeClass('active');
+		});
+	}
 });
